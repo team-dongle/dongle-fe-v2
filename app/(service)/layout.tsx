bba8daa3e6b5594184a3979e9f43712e970c4f41
조용원 <auau3@kakao.com>
2024-12-11 15:34:25 +0900

    fix: footer height 계산하여 main padding-bottom 수정 - footer 때문에 main의 내용이 가려지는 현상 - main에 padding-bottom을 주어 가려지는 현상 제거

--- app/(service)/layout.tsx
+++ app/(service)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main className="m-[0_auto] min-h-screen max-w-[896px] pt-40">
+      <main className="m-[0_auto] min-h-screen max-w-[896px] pb-52 pt-40">
         {children}
       </main>
       <Footer />
