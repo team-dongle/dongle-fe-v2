28cc546cc4bc6deab61a3cd9149f076d91556a97
조용원 <auau3@kakao.com>
2024-12-11 16:06:58 +0900

    hotfix: footer position type 변경 - footer component가 bottom에 제대로 고정이 안되는 현상 수정 - footer position type 변경: absolute -> relative

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 h-40 w-full bg-zinc-50">
+    <footer className="relative bottom-0 h-40 w-full bg-zinc-50">
       <div className="m-[0_auto] flex h-full max-w-[896px] flex-col items-start justify-center">
         <p className="text-sm font-semibold text-zinc-400">
           © by 동글. All Right Reserved.
