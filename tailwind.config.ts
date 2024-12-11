0a884935ec875f46fe01a7aaeacccf85477414d6
조용원 <auau3@kakao.com>
2024-12-11 14:28:30 +0900

    hotfix: tailwind color palette 설정 변경 - primary color를 extends object로 위치 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,21 +11,19 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-      },
-    },
-    colors: {
-      primary: {
-        "50": "#f1f8fe",
-        "100": "#e2f0fc",
-        "200": "#bee1f9",
-        "300": "#84c9f5",
-        "400": "#31a6ec", // Default Color
-        "500": "#1a94dd",
-        "600": "#0d75bc",
-        "700": "#0c5e98",
-        "800": "#0e4f7e",
-        "900": "#114369",
-        "950": "#0c2a45",
+        primary: {
+          "50": "#f1f8fe",
+          "100": "#e2f0fc",
+          "200": "#bee1f9",
+          "300": "#84c9f5",
+          "400": "#31a6ec", // Default Color
+          "500": "#1a94dd",
+          "600": "#0d75bc",
+          "700": "#0c5e98",
+          "800": "#0e4f7e",
+          "900": "#114369",
+          "950": "#0c2a45",
+        },
       },
     },
   },
